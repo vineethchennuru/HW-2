fa1b3457e773058efd8ebdb0557c4683c6894e28
Vineeth Chennuru <chennuruvineeth@gmail.com>
2023-01-31 11:35:31 -0500

    Added heading to charts

--- app.py
+++ app.py
@@ -22,5 +22,11 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+
+st.text('Close price of stock')
+
 st.line_chart(tickerDf.Close)
+
+st.text('Volume of stock')
+
 st.line_chart(tickerDf.Volume)
